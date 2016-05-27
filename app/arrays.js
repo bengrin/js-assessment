073bee46ea76a9a4c6528bd8598c9b2b14d373ab
Ben Grinberg <bengrin25@gmail.com>
2016-05-26 23:47:56 -0400

    basic array methods

--- app/arrays.js
+++ app/arrays.js
@@ -29,34 +29,39 @@ exports.arraysAnswers = {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] === item) {
         arr.splice(i, 1);
-        i--
+        i--;
       }
     }
     return arr;
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
