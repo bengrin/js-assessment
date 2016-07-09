ed410c1e33c5ee88c923287409a756e9795b4594
Ben Grinberg <bengrin25@gmail.com>
2016-07-09 19:57:00 -0400

    async

--- app/async.js
+++ app/async.js
@@ -2,7 +2,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve) => resolve(value))
   },
 
   manipulateRemoteData: function(url) {
