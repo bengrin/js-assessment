a0740cf629c4864e938b64a15c7d9b66b821e61e
Ben Grinberg <bengrin25@gmail.com>
2016-07-10 19:57:24 -0400

    numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    let binary = num.toString(2);
+      if (binary.length < 8) {
+        while (binary.length < 8) {
+          binary = '0' + binary;
+        }
+      }
+      return Number(binary.split('').reverse().join('')[bit - 1]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    let result = num.toString(2);
+    if (result.length < 8) {
+      while (result.length < 8) {
+        result = '0' + result;
+      }
+    }
+    return result;
   },
 
   multiply: function(a, b) {
-
+    let length = Math.max(a.toString().length, b.toString().length);
+    let result = (a * b).toString().split('');
+    result.length = length;
+    return Number(result.join(''));
   }
 };
