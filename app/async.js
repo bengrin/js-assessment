a728bdbd3d01bbe0c671c28c877c873e6a54856d
Ben Grinberg <bengrin25@gmail.com>
2016-07-09 20:04:26 -0400

    manipulateRemoteData

--- app/async.js
+++ app/async.js
@@ -6,6 +6,21 @@ exports.asyncAnswers = {
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise((res, rej) => {
+      $.ajax({
+        url: url,
+        type: 'GET',
+        success: (data) => {
+          let people = data.people.reduce((acc, cur) => {
+            acc.push(cur.name);
+            return acc;
+          }, []).sort();
+          res(people);
+        },
+        error: (error) => {
+          rej(error);
+        }
+      })
+    })
   }
 };
