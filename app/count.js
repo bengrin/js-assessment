e1a2997360d4b3e1e0b2c3208b5b53cb702279ea
Ben Grinberg <bengrin25@gmail.com>
2016-07-09 20:14:58 -0400

    count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let timer;
 
+    const print = function() {
+      console.log(start++);
+
+      if (start <= end) {
+        timer = setTimeout(print, 100);
+      }
+    }
+    print();
+
+    return {
+      cancel () {
+        clearTimeout(timer);
+      }
+    };
   }
 };
