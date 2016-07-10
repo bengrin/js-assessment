29237eb83560697e2b372bc65dc8cdd0bcee8ef6
Ben Grinberg <bengrin25@gmail.com>
2016-07-10 14:38:23 -0400

    regex

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return (/\d/).test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return (/([a-z])\1+/i).test(str);
   },
 
   endsWithVowel: function(str) {
-
+    return (/[a,e,i,o,u]$/i).test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    let match = (/\d{3}/).exec(str);
+    return match ? match[0] : false;
   },
 
   matchesPattern: function(str) {
-
+    return (/^\d{3}-\d{3}-\d{4}$/).test(str);
   },
   isUSD: function(str) {
-
+    return (/^\$\d{1,3}(,?\d{3})*(\.\d{2})?$/).test(str);
   }
 };
