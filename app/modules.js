91918c81c4c2d74c6967dabc32ffb43902808038
Ben Grinberg <bengrin25@gmail.com>
2016-07-09 20:55:00 -0400

    modules

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,13 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    let module = {
+      sayIt: function() {
+          return `${this.greeting}, ${this.name}`;
+      },
+      name: str2,
+      greeting: str1
+    };
+    return module;
   }
 };
