dd523be798c32cf0e7cdca60f8b5b3280f8b8cd2
Ben Grinberg <bengrin25@gmail.com>
2016-07-09 20:19:05 -0400

    flowControl

--- app/flowControl.js
+++ app/flowControl.js
@@ -10,6 +10,18 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    if (!num || typeof num !== 'number') {
+        return false;
+    }
+    let string = ''
 
+    if (num % 3 === 0) {
+        string += 'fizz';
+    }
+    if (num % 5 === 0) {
+        string += 'buzz';
+    }
+
+    return string || num;
   }
 };
